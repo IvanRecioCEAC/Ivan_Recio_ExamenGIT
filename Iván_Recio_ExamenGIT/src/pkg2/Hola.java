2548832436a4b78738f8956b15ab9a2a20bf9b66
IvanRecio-AlumnoCEAC <IvanRecio-AlumnoCEAC@CEACFP10333>
2023-05-26 12:39:29 +0200

    Actualización archivo hola.java rama prueba1

--- Iván_Recio_ExamenGIT/src/pkg2/Hola.java
+++ Iván_Recio_ExamenGIT/src/pkg2/Hola.java
@@ -1,7 +1,15 @@
 package pkg2;
-
+import java.util.*;
 public class Hola {
 	public void saludos() {
 		System.out.println("Hola muy buenas");
+	}
+	
+	public void libro() {
+		String libro;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime un libro que me recomiendes: ");
+		libro = sc.nextLine();
+		System.out.println("Prefecto me leere este libro: " + libro);
 	}
 }
