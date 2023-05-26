3ff1729dc8df85ce72582fdcb30757edcbe77f7a
IvanRecio-AlumnoCEAC <IvanRecio-AlumnoCEAC@CEACFP10333>
2023-05-26 12:32:56 +0200

    Añadimos nuevo archivo Hola.java en otro paquete

--- Iván_Recio_ExamenGIT/src/pkg/Clase.java
+++ Iván_Recio_ExamenGIT/src/pkg/Clase.java
@@ -6,16 +6,16 @@ public class Clase {
 	public void holaMuyBuenas() {
 		String nombre;
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Hola muy buenas introduzca su nombre: ");
+		System.out.println("Hola muy buenas introduzca su nombre: ");
 		nombre = sc.nextLine();
-		System.out.print("Bienvenido " + nombre);
+		System.out.println("Bienvenido " + nombre);
 	}
 	
 	public void edad() {
 		int edad;
 		Scanner sn = new Scanner(System.in);
-		System.out.print("Introduzca su edad: ");
+		System.out.println("Introduzca su edad: ");
 		edad = sn.nextInt();
-		System.out.print("Te ves muy mayor para tener " + edad + " años");
+		System.out.println("Te ves muy mayor para tener " + edad + " años");
 	}
 }
---
+++ Iván_Recio_ExamenGIT/src/pkg2/Hola.java
@@ -0,0 +1,7 @@
+package pkg2;
+
+public class Hola {
+	public void saludos() {
+		System.out.println("Hola muy buenas");
+	}
+}
